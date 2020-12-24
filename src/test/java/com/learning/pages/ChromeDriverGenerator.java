3126df71be0325075ae4954ff1476cf6934d63e5
BarteKKmita <bartekkmitaa@gmail.com>
2020-12-24 16:54:34 +0100

    Add testing.xml

--- src/test/java/com/learning/pages/ChromeDriverGenerator.java
+++ src/test/java/com/learning/pages/ChromeDriverGenerator.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class ChromeDriverGenerator {
 
   private final WebDriver driver;
-  public static final String CHROME_USER_DATA = "C://Users//Bartek//AppData//Local//Google//Chrome/User Data";
+  public static final String CHROME_USER_DATA = "C://Users//Bartek//AppData//Local//Google//Chrome//User Data";
 
   public ChromeDriverGenerator() {
     driver =new ChromeDriver(getChromeOptions());
@@ -21,6 +21,7 @@ public class ChromeDriverGenerator {
     System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
     ChromeOptions options = new ChromeOptions();
     options.addArguments("user-data-dir=" + CHROME_USER_DATA);
+    options.addArguments("profile-directory=Profile 1");
     return options;
   }
 }
