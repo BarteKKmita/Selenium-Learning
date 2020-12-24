b12b21572652739739eb3d10d2824c740af95729
BarteKKmita <bartekkmitaa@gmail.com>
2020-12-24 11:12:40 +0100

    Page object model fundamentals

--- src/test/java/com/learning/GoogleSearchTest.java
+++ src/test/java/com/learning/GoogleSearchTest.java
@@ -1,5 +1,6 @@
 package com.learning;
 
+import com.learning.pages.ChromeDriverGenerator;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
