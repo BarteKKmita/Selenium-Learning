cf0830bd7d566cea67e5564f9cfa724438e19cb1
BarteKKmita <bartekkmitaa@gmail.com>
2020-12-26 11:06:59 +0100

    TestNGListeners

---
+++ src/test/java/com/learning/listeners/ListenerTest.java
@@ -0,0 +1,26 @@
+package com.learning.listeners;
+
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners(TestNGListener.class)
+public class ListenerTest {
+
+  @Test
+  void test1(){
+    System.out.println("Doing first test.");
+  }
+
+
+  @Test
+  void test2(){
+    System.out.println("Doing second test.");
+  }
+
+
+  @Test
+  void test3(){
+    System.out.println("Doing third test.");
+  }
+
+}
