599bd3fda50de59334715b1cbf841521799fa34d
BarteKKmita <bartekkmitaa@gmail.com>
2021-01-06 14:12:45 +0100

    Add WebDriverFactory

--- src/main/java/com/learning/browsers/WebDriverFactory.java
+++ src/main/java/com/learning/browsers/WebDriverFactory.java
@@ -5,17 +5,24 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
-public class BrowserDriverGenerator implements BrowserDriver {
+public class WebDriverFactory {
 
   private static final String BROWSER_PROPERTIES_NAME = "browser";
   private static final String PATH_TO_EDGE_DRIVER = "pathToEdgeDriver";
   private final PropertiesReader propertiesReader;
 
-  public BrowserDriverGenerator() {
-    propertiesReader = new PropertiesReader();
+  private WebDriver webDriver;
+
+  public WebDriverFactory(PropertiesReader propertiesReader) {
+    this.propertiesReader = propertiesReader;
+    webDriver = generateWebDriver();
   }
 
-  public WebDriver getDriver() {
+  public WebDriver getWebDriver() {
+    return webDriver;
+  }
+
+  private WebDriver generateWebDriver() {
     String browserName = propertiesReader.getProperty(BROWSER_PROPERTIES_NAME);
     if (browserName.equalsIgnoreCase("Chrome")) {
       return new ChromeDriver();
--- src/main/java/com/learning/pages/PagesModule.java
+++ src/main/java/com/learning/pages/PagesModule.java
@@ -4,18 +4,18 @@ import com.google.inject.Exposed;
 import com.google.inject.PrivateModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
-import com.learning.browsers.BrowserDriverGenerator;
 import com.learning.browsers.DriverModule;
 import com.learning.configuration.PropertiesReader;
 import com.learning.pages.google.GoogleSearchPage;
+import org.openqa.selenium.WebDriver;
 
 public class PagesModule extends PrivateModule {
 
   @Provides
   @Exposed
   @Singleton
-  public GoogleSearchPage getGoogleSearchPage(BrowserDriverGenerator driverGenerator, PropertiesReader propertiesReader) {
-    return new GoogleSearchPage(driverGenerator.getWebDriver(), propertiesReader);
+  public GoogleSearchPage getGoogleSearchPage(WebDriver webDriver, PropertiesReader propertiesReader) {
+    return new GoogleSearchPage(webDriver, propertiesReader);
   }
 
   @Override
