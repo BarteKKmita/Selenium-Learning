73321d0a74a61c09ceed2279dcdb3c31691f9d79
BarteKKmita <bartekkmitaa@gmail.com>
2021-01-06 16:26:03 +0100

    Add closing webdriver

--- src/main/java/com/learning/pages/PageBase.java
+++ src/main/java/com/learning/pages/PageBase.java
@@ -1,15 +1,21 @@
 package com.learning.pages;
 
+import java.io.Closeable;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
-public abstract class PageBase {
+public abstract class PageBase implements Closeable {
 
   protected final WebDriver driver;
 
   public PageBase(WebDriver driver) {
     this.driver=driver;
     PageFactory.initElements(driver, this);
+  }
+
+  public void close() {
+    driver.close();
+    driver.quit();
   }
 
   public abstract boolean waitForPageToLoad();
