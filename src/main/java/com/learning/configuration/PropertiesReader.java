b37a63c891b89ed6de355236bf0cf78ba87d8eb3
BarteKKmita <bartekkmitaa@gmail.com>
2021-01-06 14:01:57 +0100

    Add class loader to PropertiesReader

--- src/main/java/com/learning/configuration/PropertiesReader.java
+++ src/main/java/com/learning/configuration/PropertiesReader.java
@@ -1,26 +1,19 @@
 package com.learning.configuration;
 
-import com.google.inject.Singleton;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Singleton
 public class PropertiesReader {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(PropertiesReader.class);
-  private final String pathToFile;
-
-  public PropertiesReader() {
-    pathToFile = "src/main/resources/testProperties.yaml";
-  }
+  private static final String PATH_TO_FILE = "testProperties.yaml";
 
   public String getProperty(String property) {
     String readProperty = "";
-    try (InputStream input = new FileInputStream(pathToFile)) {
+    try (InputStream input = getPropertiesFileAsStream()) {
       Properties propertiesReader = new Properties();
       propertiesReader.load(input);
       readProperty = propertiesReader.getProperty(property);
@@ -31,5 +24,11 @@ public class PropertiesReader {
       System.exit(1);
     }
     return readProperty;
+  }
+
+  private InputStream getPropertiesFileAsStream() {
+    return this.getClass()
+        .getClassLoader()
+        .getResourceAsStream(PATH_TO_FILE);
   }
 }
