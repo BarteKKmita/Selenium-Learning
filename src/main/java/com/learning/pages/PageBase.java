fc86e4101ab72dabb82635ec742bcca4afd957b9
BarteKKmita <bartekkmitaa@gmail.com>
2021-01-06 14:15:11 +0100

    Add WebDriver as field to page objects

--- src/main/java/com/learning/pages/PageBase.java
+++ src/main/java/com/learning/pages/PageBase.java
@@ -4,9 +4,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
 public abstract class PageBase {
+
+  protected final WebDriver driver;
+
   public PageBase(WebDriver driver) {
+    this.driver=driver;
     PageFactory.initElements(driver, this);
   }
 
-  public abstract void waitForPageToLoad(WebDriver driver);
+  public abstract boolean waitForPageToLoad();
 }
