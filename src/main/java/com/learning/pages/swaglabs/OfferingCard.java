c47366f861de184aceca665b8a9be9120349047e
BarteKKmita <bartekkmitaa@gmail.com>
2021-01-09 13:13:07 +0100

    Add Inventory Page

---
+++ src/main/java/com/learning/pages/swaglabs/OfferingCard.java
@@ -0,0 +1,27 @@
+package com.learning.pages.swaglabs;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.DefaultElementLocatorFactory;
+
+public class OfferingCard {
+
+  @FindBy(css = "button[class*=btn_primary")
+  private WebElement addToCartButton;
+
+  @FindBy(className = "inventory_item_name")
+  private WebElement itemName;
+
+  public OfferingCard(WebElement rootElement) {
+    PageFactory.initElements(new DefaultElementLocatorFactory(rootElement), this);
+  }
+
+  public void addToCart() {
+    addToCartButton.click();
+  }
+
+  public String getItemName() {
+    return itemName.getText();
+  }
+}
